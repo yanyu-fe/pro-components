0c9da479769b0ba9ae26780105a5ab74fbb2860f
zhuzhengjian <aibyanyu@qq.com>
2022-01-01 07:47:49 +0800

    fix: 增加wave样式代码

--- packages/components/wave/src/wave.tsx
+++ packages/components/wave/src/wave.tsx
@@ -4,13 +4,14 @@ import "./style";
 import { useBindAnimation } from "./hooks/useAnimation";
 export default defineComponent({
   name: "ProWave",
+  props: ["insertExtraNode"],
   setup(_props, { slots }) {
     let node;
     onMounted(() => {
       // 拿到当前的实例
       node = useGetInstance();
       // 绑定监听
-      useBindAnimation(node as Element);
+      useBindAnimation(_props, node as Element);
     });
     return () => {
       return slots.default && slots.default();
